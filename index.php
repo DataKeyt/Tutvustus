ff5fa48c8da193e8bac6471d33e815c73b7f2ca6
Rajur <rajur.hint@gmail.com>
2015-09-08 16:36:03 +0100

    Pildi lisamine avalehele [Delivers #102579594]

---
+++ index.php
@@ -0,0 +1,55 @@
+
+<!--
+ * Created by PhpStorm.
+ * User: Rajur
+ * Date: 07-9-15
+ * Time: 12:04
+ -->
+<!DOCTYPE html>
+    <html>
+        <head>
+            <title>KES MA OLEN?</title>
+            <link rel="stylesheet" type="text/css" href="stylesheet.css">
+        <head>
+            <body>
+                <h1 class="title">kes ma olen?</h1>
+                <div class="photo">
+                    <a href="https://www.facebook.com/retekeyt">
+                        <img src="img/photo.jpg"/>
+                    </a>
+                    <p><a href=" http://codecademy.com/keybot/">"Codecademy"</a></p>
+                </div>
+                <table>
+                    <tr>
+                        <th>Ajaperiood</th>
+                        <th>Tegevused</th>
+                    </tr>
+                    <tr>
+                        <td>Nädal</td>
+                        <td>Õppimine</td>
+                    </tr>
+                </table>
+                <div>
+                    <h2>Tutvustus</h2>
+                    <p>Olen kahekümnendates neiu ja õpin Tartu Kutsehariduskeskuses</p>
+                    <h2>Hobbid</h2>
+                    <p>Mulle meeldib ujuda, jalutada ja rattaga sõita</p>
+                    <h2>Info foto kohta</h2>
+                    <p>Pilt on tehtud KHK õmbleja eriala lõpetamisel</p>
+                </div>
+                <div>
+                    <div class="item">
+                        <p>Rete- Keyt Uibo</p>
+                        <p>25 Narva mnt. Elva 43550</p>
+                    </div>
+                    <div class="item">
+                        <p>Tel. 54678900</p>
+                        <p>E-mail: thunderbird@gmail.com</p>
+                    </div>
+                    <div class="item">
+                        <p>Skype: Rete-Keyt</p>
+                        <p>Twitter: https://twitter.com/rete-keyt</p>
+                    </div>
+                </div>
+            </body>
+     </html>
