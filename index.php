399830d914c7e0891c023ebba7f8786af5b8feb8
Rajur <rajur.hint@gmail.com>
2015-09-09 12:29:36 +0100

    first commit. sisu lisatud [Delivers #102658380]

--- index.php
+++ index.php
@@ -17,7 +17,25 @@
                     <h2>Tutvustus</h2>
                     <p>Olen üks 19- aastane Elvast pärit rohesilmne tütarlaps, kellele meeldib vabal ajal raamatuid lugeda, end Codeacademys veebiarenduse alal arendada, Ingressi mängida ja palju muud toredat korda saata.
                         Mul ei ole palju lemmiktegevusi, -sööke ega muid lemmikuid, kuid see ei tähenda, et ühtegi lemmikasja poleks. Nii et natukene lemmikmängust. Ingress- see on mäng, kus terve maailm on meie mängulaud.
-                        Sõna otseses mõttes. Ingressi ei saa mängida toas istudes. Selleks tuleb õues ringi liikuda kas ratta, auto või kondimootori abil. Rohkem infot selle kohta on olemas siin</p>
+                        Sõna otseses mõttes. Ingressi ei saa mängida toas istudes. Selleks tuleb õues ringi liikuda kas ratta, auto või kondimootori abil. Rohkem infot selle kohta on olemas <a href="http://www.arvustus.com/ingress-eesti">siin</a></p>
+                    <h2>TAUST</h2>
+                    <h2>Eelmised koolid</h2>
+                    <ul>
+                        <li>Tartu Kutsehariduskeskus</li>
+                        <li>Elva Gümnassium</li>
+                        <p>Õppisin enne praegusele erialale kandideerimist kolm aastat rõivaõmblust. Lõpetasin kiitusega ja käisin fancydel vastuvõttudel.
+
+                        Paljud imestavad ja küsivad, miks selline küllaltki ootamatu otsus. Põhjuseid on mitmeid:
+                        <ol>
+                            <li>Suur huvi veebiarenduse vastu</li>
+                            <li>Soov kunagi sel alal töötada</li>
+                            <li>Miks mitte?</li>
+                        </ol>
+                        Erialane taust ei ole veel väga märkimisväärne. Hakksin kevadel ühe väga hea sõbra abil õppima HTML-i ja CSS-i. Väga kaugele sellega kahjuks ei jõudnud, kuna eksamid tulid vahele ning fookust oli mujal vaja.
+
+                        Juunis läbisin Codeacademys "Make a Website" kursuse. Veidi aega hiljem ka "Make an Interactive Website" kursuse, kus oli esimene kokkupuude JavaScriptiga(mis väidetavalt pidi imelihtne olema!). Hetkel on pooleli kaks kursust, aga mitte kauaks
+                        </p>
+                    </ul>
                     <h2>Hobbid</h2>
                     <p>Mulle meeldib ujuda, jalutada ja rattaga sõita</p>
                     <h2>Info foto kohta</h2>
