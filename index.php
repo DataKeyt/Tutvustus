27dd0de3132fcb96a6550a07e4294f764e20c6f3
Rajur <rajur.hint@gmail.com>
2015-09-09 17:50:43 +0100

    first commit. sisu lisatud [Delivers #102658380]

--- index.php
+++ index.php
@@ -15,40 +15,49 @@
                 <h1 class="title">kes ma olen?</h1>
                 <div class="tutvustus">
                     <h2>Tutvustus</h2>
-                    <p>Olen üks 19- aastane Elvast pärit rohesilmne tütarlaps, kellele meeldib vabal ajal raamatuid lugeda, end Codeacademys veebiarenduse alal arendada, Ingressi mängida ja palju muud toredat korda saata.
-                        Mul ei ole palju lemmiktegevusi, -sööke ega muid lemmikuid, kuid see ei tähenda, et ühtegi lemmikasja poleks. Nii et natukene lemmikmängust. Ingress- see on mäng, kus terve maailm on meie mängulaud.
-                        Sõna otseses mõttes. Ingressi ei saa mängida toas istudes. Selleks tuleb õues ringi liikuda kas ratta, auto või kondimootori abil. Rohkem infot selle kohta on olemas <a href="http://www.arvustus.com/ingress-eesti">siin</a></p>
+                    <p>Olen üks 19- aastane Elvast pärit rohesilmne tütarlaps, kellele meeldib vabal ajal raamatuid lugeda,
+                        end Codeacademys veebiarenduse alal arendada, Ingressi mängida ja palju muud toredat korda saata.
+                    </p>
+                        <br>
+                        <p>Mul ei ole palju lemmiktegevusi, -sööke ega muid lemmikuid,
+                            kuid see ei tähenda, et ühtegi lemmikasja poleks. Nii et natukene lemmikmängust.
+                            Ingress- see on mäng, kus terve maailm on meie mängulaud.
+                            Sõna otseses mõttes. Ingressi ei saa mängida toas istudes.
+                            Selleks tuleb õues ringi liikuda kas ratta, auto või kondimootori abil.
+                            Rohkem infot selle kohta on olemas <a href="http://www.arvustus.com/ingress-eesti">siin</a>
+                        </p>
                     <h2>TAUST</h2>
+                        <br>
                     <h2>Eelmised koolid</h2>
+                        <br>
                     <ul>
                         <li>Tartu Kutsehariduskeskus</li>
                         <li>Elva Gümnassium</li>
-                        <p>Õppisin enne praegusele erialale kandideerimist kolm aastat rõivaõmblust. Lõpetasin kiitusega ja käisin fancydel vastuvõttudel.
-
+                    </ul>
+                        <p>Õppisin enne praegusele erialale kandideerimist kolm aastat rõivaõmblust. Lõpetasin kiitusega ja käisin fancydel vastuvõttudel.</p>
+                            <br>
                         Paljud imestavad ja küsivad, miks selline küllaltki ootamatu otsus. Põhjuseid on mitmeid:
                         <ol>
                             <li>Suur huvi veebiarenduse vastu</li>
                             <li>Soov kunagi sel alal töötada</li>
                             <li>Miks mitte?</li>
                         </ol>
-                        Erialane taust ei ole veel väga märkimisväärne. Hakksin kevadel ühe väga hea sõbra abil õppima HTML-i ja CSS-i. Väga kaugele sellega kahjuks ei jõudnud, kuna eksamid tulid vahele ning fookust oli mujal vaja.
-
-                        Juunis läbisin Codeacademys "Make a Website" kursuse. Veidi aega hiljem ka "Make an Interactive Website" kursuse, kus oli esimene kokkupuude JavaScriptiga(mis väidetavalt pidi imelihtne olema!). Hetkel on pooleli kaks kursust, aga mitte kauaks
+                        <br>
+                        <p>Erialane taust ei ole veel väga märkimisväärne. Hakkasin kevadel ühe väga hea sõbra abil õppima HTML-i ja CSS-i.
+                        Väga kaugele sellega kahjuks ei jõudnud, kuna eksamid tulid vahele ning fookust oli mujal vaja.</p>
+                        <br>
+                        <p>Juunis läbisin Codeacademys "Make a Website" kursuse. Veidi aega hiljem ka "Make an Interactive Website" kursuse,
+                        kus oli esimene kokkupuude JavaScriptiga(mis väidetavalt pidi imelihtne olema!). Hetkel on pooleli kaks kursust, aga mitte kauaks
                         </p>
-                    </ul>
-                    <h2>Hobbid</h2>
-                    <p>Mulle meeldib ujuda, jalutada ja rattaga sõita</p>
-                    <h2>Info foto kohta</h2>
-                    <p>Pilt on tehtud KHK õmbleja eriala lõpet amisel</p>
+
                 </div>
-                    <div class="photo"><a href="https://www.facebook.com/retekeyt">
-                        <img src="img/photo.jpg"/>
-                    </a>
-                    <p><a href=" http://codecademy.com/keybot/">"Codecademy"</a></p>
+                    <div class="photo">
+                        <a href="https://www.facebook.com/retekeyt"><img src="img/photo.jpg"/></a>
+                        <p><a href=" http://codecademy.com/keybot/">"Codecademy"</a></p>
+                    </div>
                 <div class="tabel">
                     <table>
-                        <table border="1">
-                    <tr>
+                        <tr>
                         <th>Ajaperiood</th>
                         <th>Tegevused</th>
                     </tr>
@@ -61,16 +70,16 @@
 
                 <div>
                     <div class="item">
-                        <p>Rete- Keyt Uibo</p>
-                        <p>25 Narva mnt. Elva 43550</p>
+                        <p>Nimi: Rete-Keyt Uibo</p>
+                        <p>Telefon: +372 5681 1269</p>
                     </div>
                     <div class="item">
-                        <p>Tel. 54678900</p>
-                        <p>E-mail: thunderbird@gmail.com</p>
+                        <p>Kooli e-mail: rete-keyt.uibo@khk.ee</p>
+                        <p>Isiklik e-mail: retekeyt@gmail.com</p>
                     </div>
                     <div class="item">
-                        <p>Skype: Rete-Keyt</p>
-                        <p>Twitter: https://twitter.com/rete-keyt</p>
+                        <p>Twitter: https://twitter.com/DataKeyt</p>
+                        <p>Facbook: https://www.facebook.com/retekeyt</p>
                     </div>
                 </div>
             </body>
