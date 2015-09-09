8ae17bd8547a67475d349e8439f0ad17c0ee8b12
Rajur <rajur.hint@gmail.com>
2015-09-09 18:27:24 +0100

    main structure [Delivers #102658380]

--- index.php
+++ index.php
@@ -12,63 +12,66 @@
             <link rel="stylesheet" type="text/css" href="stylesheet.css">
         <head>
             <body>
-                <h1 class="title">kes ma olen?</h1>
-                <div class="tutvustus">
-                    <h2>Tutvustus</h2>
-                    <p>Olen üks 19- aastane Elvast pärit rohesilmne tütarlaps, kellele meeldib vabal ajal raamatuid lugeda,
-                        end Codeacademys veebiarenduse alal arendada, Ingressi mängida ja palju muud toredat korda saata.
-                    </p>
-                        <br>
-                        <p>Mul ei ole palju lemmiktegevusi, -sööke ega muid lemmikuid,
-                            kuid see ei tähenda, et ühtegi lemmikasja poleks. Nii et natukene lemmikmängust.
-                            Ingress- see on mäng, kus terve maailm on meie mängulaud.
-                            Sõna otseses mõttes. Ingressi ei saa mängida toas istudes.
-                            Selleks tuleb õues ringi liikuda kas ratta, auto või kondimootori abil.
-                            Rohkem infot selle kohta on olemas <a href="http://www.arvustus.com/ingress-eesti">siin</a>
+                <div class="wrapper">
+                    <h1 class="title">kes ma olen?</h1>
+                    <div class="tutvustus">
+                        <h2>Tutvustus</h2>
+                        <p>Olen üks 19- aastane Elvast pärit rohesilmne tütarlaps, kellele meeldib vabal ajal raamatuid lugeda,
+                            end Codeacademys veebiarenduse alal arendada, Ingressi mängida ja palju muud toredat korda saata.
                         </p>
-                    <h2>TAUST</h2>
-                        <br>
-                    <h2>Eelmised koolid</h2>
-                        <br>
-                    <ul>
-                        <li>Tartu Kutsehariduskeskus</li>
-                        <li>Elva Gümnassium</li>
-                    </ul>
-                        <p>Õppisin enne praegusele erialale kandideerimist kolm aastat rõivaõmblust. Lõpetasin kiitusega ja käisin fancydel vastuvõttudel.</p>
                             <br>
-                        Paljud imestavad ja küsivad, miks selline küllaltki ootamatu otsus. Põhjuseid on mitmeid:
-                        <ol>
-                            <li>Suur huvi veebiarenduse vastu</li>
-                            <li>Soov kunagi sel alal töötada</li>
-                            <li>Miks mitte?</li>
-                        </ol>
-                        <br>
-                        <p>Erialane taust ei ole veel väga märkimisväärne. Hakkasin kevadel ühe väga hea sõbra abil õppima HTML-i ja CSS-i.
-                        Väga kaugele sellega kahjuks ei jõudnud, kuna eksamid tulid vahele ning fookust oli mujal vaja.</p>
-                        <br>
-                        <p>Juunis läbisin Codeacademys "Make a Website" kursuse. Veidi aega hiljem ka "Make an Interactive Website" kursuse,
-                        kus oli esimene kokkupuude JavaScriptiga(mis väidetavalt pidi imelihtne olema!). Hetkel on pooleli kaks kursust, aga mitte kauaks
-                        </p>
+                            <p>Mul ei ole palju lemmiktegevusi, -sööke ega muid lemmikuid,
+                                kuid see ei tähenda, et ühtegi lemmikasja poleks. Nii et natukene lemmikmängust.
+                                Ingress- see on mäng, kus terve maailm on meie mängulaud.
+                                Sõna otseses mõttes. Ingressi ei saa mängida toas istudes.
+                                Selleks tuleb õues ringi liikuda kas ratta, auto või kondimootori abil.
+                                Rohkem infot selle kohta on olemas <a href="http://www.arvustus.com/ingress-eesti">siin</a>
+                            </p>
+                        <h2>TAUST</h2>
+                            <br>
+                        <h2>Eelmised koolid</h2>
+                            <br>
+                        <ul>
+                            <li>Tartu Kutsehariduskeskus</li>
+                            <li>Elva Gümnassium</li>
+                        </ul>
+                            <p>Õppisin enne praegusele erialale kandideerimist kolm aastat rõivaõmblust. Lõpetasin kiitusega ja käisin fancydel vastuvõttudel.</p>
+                                <br>
+                            Paljud imestavad ja küsivad, miks selline küllaltki ootamatu otsus. Põhjuseid on mitmeid:
+                            <ol>
+                                <li>Suur huvi veebiarenduse vastu</li>
+                                <li>Soov kunagi sel alal töötada</li>
+                                <li>Miks mitte?</li>
+                            </ol>
+                            <br>
+                            <p>Erialane taust ei ole veel väga märkimisväärne. Hakkasin kevadel ühe väga hea sõbra abil õppima HTML-i ja CSS-i.
+                            Väga kaugele sellega kahjuks ei jõudnud, kuna eksamid tulid vahele ning fookust oli mujal vaja.</p>
+                            <br>
+                            <p>Juunis läbisin Codeacademys "Make a Website" kursuse. Veidi aega hiljem ka "Make an Interactive Website" kursuse,
+                            kus oli esimene kokkupuude JavaScriptiga(mis väidetavalt pidi imelihtne olema!). Hetkel on pooleli kaks kursust, aga mitte kauaks
+                            </p>
 
-                </div>
-                    <div class="photo">
-                        <a href="https://www.facebook.com/retekeyt"><img src="https://s3.amazonaws.com/prod.tracker2/resource/50481576/IMG_0162edited.jpg?AWSAccessKeyId=AKIAJEX3ET63U5T77TYA&Expires=1441819528&Signature=%2FlO3GRLKRIlu7pSVmaOqDjX5NE0%3D"/></a>
-                        <p><a href=" http://codecademy.com/keybot/">"Codecademy"</a></p>
                     </div>
-                <div class="tabel">
-                    <table>
-                        <tr>
-                        <th>Ajaperiood</th>
-                        <th>Tegevused</th>
-                    </tr>
-                    <tr>
-                        <td>Nädal</td>
-                        <td>Õppimine</td>
-                    </tr>
-                </table>
-                </div>
-
-                <div>
+                    <div class="right">
+                        <div class="photo">
+                            <a href="https://www.facebook.com/retekeyt"><img src="https://s3.amazonaws.com/prod.tracker2/resource/50481576/IMG_0162edited.jpg?AWSAccessKeyId=AKIAJEX3ET63U5T77TYA&Expires=1441819528&Signature=%2FlO3GRLKRIlu7pSVmaOqDjX5NE0%3D"/></a>
+                            <p><a href=" http://codecademy.com/keybot/">"Codecademy"</a></p>
+                        </div>
+                        <div class="tabel">
+                            <table>
+                                <tr>
+                                    <th>Ajaperiood</th>
+                                    <th>Tegevused</th>
+                                </tr>
+                                <tr>
+                                    <td>Nädal</td>
+                                    <td>Õppimine</td>
+                                </tr>
+                            </table>
+                        </div> <!-- .tabel -->
+                    </div>
+                </div> <!-- .wrapper -->
+                <div class="contact">
                     <div class="item">
                         <p>Nimi: Rete-Keyt Uibo</p>
                         <p>Telefon: +372 5681 1269</p>
